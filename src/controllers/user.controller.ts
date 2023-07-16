5e9f42178069ff60531da9cca5cc91d33df938b0
amitjalui <amitjalui5@gmail.com>
2023-07-16 15:45:32 +0530

    get user api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,29 +1,57 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/user.model';
 
-interface ReturnResponse {
+interface ReturnreqResonse {
   status: "success" | "error",
   message: String,
   data: {}
 }
 
-const registerUser = async (req: Request, res: Response, next: NextFunction) => {
-  let resp: ReturnResponse;
-  
+let reqRes: ReturnreqResonse;
+
+const registerUser = async (req: Request, res: Response) => {
   try {
     const user = await new User(req.body).save();
 
     if (!user) {
-      const resp = { status: "error", message: "No result found", data: {} };
-      return res.json(resp);
+      reqRes = { status: "error", message: "No result found", data: {} };
+      return res.send(reqRes);
     }
     
-    resp = {status: "success", message: "Registration done", data: {userId: user._id}};
-    return res.send(resp);
+    reqRes = {status: "success", message: "Registration done", data: {userId: user._id}};
+    return res.send(reqRes);
   } catch (error) {
-    resp = {status: "error", message: "Something went wrong", data: {}};
-    return res.status(500).json(resp);
+    reqRes = {status: "error", message: "Something went wrong", data: {}};
+    return res.status(500).send(reqRes);
   }
 }
 
-export { registerUser };
+const getUser = async(req: Request, res:  Response) => {
+  try {
+    const userId = req.params.userId;
+
+    /*
+      @MongoDB_Projection 
+      * Allows which data needed to be show and which not
+      e.g. (userId,{name: 1, email: 1, _id: 0})
+    */
+    const user = await User.findById(userId, {name: 1, email: 1});  
+
+    if (!user) {
+      reqRes = {status: "error", message: "No user found", data: {}};
+      return res.send(reqRes);
+    } else {
+      reqRes = {status: "success", message: "User found", data: {user: user}};
+      return res.send(reqRes);
+    }
+  } catch (error) {
+    reqRes = {status: "error", message: "Something went wrong", data: {}};
+    return res.status(500).send(reqRes);
+  }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+
+}
+
+export { registerUser, getUser, updateUser };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,15 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controller';
+import { registerUser, getUser, updateUser } from '../controllers/user.controller';
 
 const userRouter = express.Router();
 
+// GET /user/:userId
+userRouter.get('/:userId', getUser);
+
+// POST /user/
 userRouter.post('/', registerUser);
+
+// PUT /user/:userId
+userRouter.put('/:userId', updateUser);
 
 export default userRouter;
